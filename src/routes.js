a20304419798ce96d4ea52240fa96085f1b3cabc
Guilherme Leite <guilhermegals@gmail.com>
2020-07-19 12:38:26 -0300

    Add auth check

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.get('/health', (req, res) => res.json({ message: 'Ok' }));
 
 routes.post('/users', UserController.create);
+routes.put('/users', authMiddleware, UserController.update);
+
 routes.post('/sessions', SessionController.create);
 
 export default routes;
