be929b1fcacdbd8c9359a0c4414f00dbf26c7516
Guilherme Leite <guilhermegals@gmail.com>
2020-07-18 22:08:44 -0300

    Add User Model and database connection

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
 routes.get('/health', (req, res) => res.json({ message: 'Ok' }));
+
+routes.get('/users', async (req, res) => {
+  const user = await User.create({
+    name: 'Guilherme Augusto',
+    email: 'guilherme@email.com',
+    password_hash: 'password123',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
